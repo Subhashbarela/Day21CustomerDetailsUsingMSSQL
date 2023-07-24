85d6bce31fe9dff07e6e57ab1fb2d17bc9f19c53
Subhash Barela <barelasubhash16@gmail.com>
2023-07-24 11:04:31 +0530

    [subhash] initial commit

---
+++ Day21CreateCustomerTableUsingDB/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day21CreateCustomerTableUsingDB
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerClass customer = new CustomerClass();
+            customer.Customer_Name = "Rajvir";
+            customer.PhoneNumber = 8654357756;
+            customer.Address = "Rajstan";
+            customer.Country = "India";
+            customer.Salary = 4535353;
+            customer.Pincode = 543435;
+            CustomerInfo info = new CustomerInfo();
+            info.InsertDataFromCustomerClass(customer);
+        }
+    }
+}
